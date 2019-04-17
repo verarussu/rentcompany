8a147f3f15dc822796b8d7930429634336fd7cac
verar <verar@LAPTOP-RM19GEGV>
2019-04-17 20:13:50 +0300

    Initial code

---
+++ RentCompanyRelease/src/telran/accounting/management/domain/repo/AccountRepository.java
@@ -0,0 +1,10 @@
+package telran.accounting.management.domain.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import telran.accounting.management.domain.Account;
+
+public interface AccountRepository extends 
+MongoRepository<Account, String> {
+
+}
---
+++ RentCompanyRelease/src/telran/cars/repo/ModelRepository.java
@@ -0,0 +1,10 @@
+package telran.cars.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import telran.cars.entities.ModelJpa;
+
+public interface ModelRepository extends
+JpaRepository<ModelJpa, String> {
+
+}
